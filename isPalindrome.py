b3d98c781094c20e35cfca55382b5233e7caa8ef
Ian Yurychuk <iyurychu@ualberta.ca>
2025-08-16 10:56:32 -0600

    Added MaxArea and ClimbStairs.

--- isPalindrome.py
+++ isPalindrome.py
@@ -12,6 +12,21 @@ def isAnagram(s: str, t: str) -> bool:
         return False
     return True
 
+def isPalindrome(s: str) -> bool:
+    print(len(s))
+    new_s = ""
+    for c in s:
+        if c.isalnum():
+            new_s = new_s + c
+    print(len(new_s))
+
+    for i in range(int(len(new_s)/2)):
+        print("Current char = ", new_s[i], ", Opposing char = ", new_s[-(i+1)])
+        if new_s[i].lower() != new_s[-(i+1)].lower():
+            return False
+    return True
+
+
 if __name__ == "__main__":
     if isAnagram("jar", "jam"):
         print("Pass")
